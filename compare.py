2e5e10474e95e840f98904b1fc9cdaf4b552935c
Rocky Shi <115847633+RockyS-07@users.noreply.github.com>
2026-05-05 19:12:32 -0400

    week 6 and small updates

--- compare.py
+++ compare.py
@@ -1,11 +1,6 @@
-from cs50 import get_int
+s = input('s: ')
 
-x = get_int("x: ")
-y = get_int("y: ")
+t = s.capitalize()
 
-if  x < y:
-    print("x is less than y")
-elif x > y:
-    print("x is greater than y")
-else:
-    print("x is equal to y")
+print(f's: {s}')
+print(f't: {t}')
